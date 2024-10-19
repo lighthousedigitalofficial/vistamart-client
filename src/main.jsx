4646cdc6b8fe464bd70fc4e035dca6ab13b9afe9
naimatofficial <naimat.official.10@gmail.com>
2024-10-19 13:09:05 +0500

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './App.css'
-import { Toaster, toast } from 'react-hot-toast';
-
-const MultiStepForm = () => {
-  const handleSubmit = () => {
-    // Example toast usage
-    toast.success('Form submitted successfully!');
-  };
-
-  return (
-    <div>
-      {/* Other form elements */}
-      <button onClick={handleSubmit}>Submit</button>
-
-      {/* Toaster component to display toast notifications */}
-      <Toaster />
-    </div>
-  );
-};
-
-export default MultiStepForm;
+import { Toaster } from 'react-hot-toast'
 
 import { ThemeProvider } from '@material-tailwind/react'
 import { RouterProvider } from 'react-router-dom'
