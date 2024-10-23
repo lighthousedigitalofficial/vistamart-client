31987f0f8f4b3a75424ee3f447df44efc4b8bcba
naimatofficial <naimat.official.10@gmail.com>
2024-10-23 11:47:11 +0500

    port change

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    // server: {
-    //     port: 6000,
-    // },
+    server: {
+        port: 80,
+    },
     base: './',
     plugins: [react()],
     build: {
